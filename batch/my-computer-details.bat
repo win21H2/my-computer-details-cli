942e757b9352a7fdaee39cb7261563a8ee24480f
324Hz <mark@cosimo.fund>
2022-07-06 17:17:56 -0500

    commit (before it breaks)

--- batch/my-computer-details.bat
+++ batch/my-computer-details.bat
@@ -2,18 +2,26 @@ ECHO OFF
 CLS
 :MENU
 ECHO.
-ECHO [32mENTER 1, 2 OR 3 TO SELECT YOUR TASK, OR 4 TO EXIT[0m
+ECHO.
+ECHO [91mMade by 324hz under the MIT license[0m
+ECHO.
+ECHO [92mhttps://github.com/win21H2/my-computer-details[0m
+ECHO.
+ECHO [32mENTER 1, 2, 3 OR 4 TO SELECT YOUR TASK, OR 5 TO EXIT[0m
 ECHO.
 ECHO 1 { BIOS INFORMATION }
 ECHO 2 { HARDWARE INFORMATION }
 ECHO 3 { WINDOWS INFORMATION }
-ECHO 4 { EXIT }
+ECHO 4 { OTHERS }
+ECHO 5 { EXIT }
 ECHO.
-SET /P M=TYPE 1, 2, 3, OR 4 THEN PRESS ENTER:
+SET /P M=TYPE 1, 2, 3, 4 OR 5, THEN PRESS ENTER:
 IF %M%==1 GOTO BIOS
 IF %M%==2 GOTO HARDWARE
 IF %M%==3 GOTO WINDOWS
-IF %M%==4 GOTO EOF
+IF %M%==4 GOTO OTHERS
+IF %M%==5 GOTO EOF
+ELSE CLS GOTO MENU
 
 :BIOS
     ECHO. 
@@ -70,8 +78,6 @@ GOTO MENU
     ECHO [34mWINDOWS STARTUP[0m
     wmic startup list brief
     ECHO.
-    ECHO [34mWINDOWS [0m
-    ECHO.
     ECHO [91mMade by 324hz under the MIT license[0m
     ECHO.
     ECHO.
@@ -80,6 +86,24 @@ GOTO MENU
 :: http://net-informations.com/q/mis/wmic.html#:~:text=WMIC%20is%20the%20abbreviation%20of,interfaces%20and%20through%20batch%20scripts.
 GOTO MENU
 
+:OTHERS
+    ECHO. 
+    ECHO [36mAPPLICATIONS[0m 
+    ECHO [31mPLEASE WAIT WHILST THE LIST OF APPLICATIONS ARE LOADING[0m 
+    ECHO [31mDO NOT TYPE ANYTHING ELSE![0m
+    wmic product get name,version
+    ECHO.
+    ECHO [91mMade by 324hz under the MIT license[0m
+    ECHO.
+    ECHO.
+GOTO MENU
+
 :EOF
-CLS
-EXIT
+SET /p choice=Are you sure you want to exit? [y/n]
+IF '%choice%'=='y' (
+    CLS
+    EXIT
+) ELSE (
+    CLS
+    GOTO MENU
+)
